585849b2ddd33c786a1427683037b37bfa4354e7
nathangreiling <ngreiling20@gmail.com>
2022-06-25 00:05:10 -0500

    service workers/indexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -4,14 +4,14 @@ const request = indexedDB.open('budget_app', 1);
 
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('new_payment', { autoIncrement: true});
+    db.createObjectStore('new_transaction', { autoIncrement: true});
 };
 
 request.onsuccess = function(event) {
     db = event.target.result;
 
     if (navigator.onLine) {
-
+        uploadTransaction();
     }
 };
 
@@ -20,7 +20,52 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-    const transaction = db.transaction(['new_payment'], 'readwrite');
-    const paymentObjectStore = transaction.objetStore('new_payment');
-    paymentObjectStore.add(record);
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objetStore('new_transaction');
+    transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access your object store
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+  
+    // get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          
+          const transactionObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          transactionObjectStore.clear();
+
+          alert('All saved payments have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  }
+
+  window.addEventListener('online', uploadTransaction)
